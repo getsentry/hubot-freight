751f679fbec6705482d083ef130d3d18a5db5c55
David Cramer <dcramer@gmail.com>
2015-07-14 17:00:08 -0700

    Add rollback command

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -8,13 +8,36 @@
 // Commands:
 //   hubot deploy <app>:<revision> to <environment>
 //   hubot cancel <app>/<env>#<number>
+//   hubot rollback <app>/<env>
+
+var apiRequest = function(msg, path, method, data) {
+  var url = process.env.HUBOT_FREIGHT_URL + path
+  var body = JSON.stringify(data || {});
+
+  var req = msg.http(url)
+    .header("Accept", "application/json")
+    .header("Authorization", "Key " + process.env.HUBOT_FREIGHT_API_KEY)
+    .header("Content-Type", "application/json");
+
+  console.log("Freight: Making request to %s with params %j", url, data);
+  return req[method.toLowerCase()](body)(function(err, resp, body) {
+      if (err) {
+        msg.reply("Freight says: " + err);
+        console.error('HTTP %d: %s', resp.statusCode, err);
+      } else if (200 <= resp.statusCode && resp.statusCode < 400) {
+        return;
+      } else {
+        var data = JSON.parse(body);
+        msg.reply("Freight responded with HTTP " + resp.statusCode + ": " + data.error);
+        console.error("HTTP %d: %s", resp.statusCode, body);
+      }
+    });
+};
 
 var deployRevision = function(msg) {
   var app = msg.match[1];
   var ref = msg.match[3];
   var env = msg.match[5];
-
-  var url = process.env.HUBOT_FREIGHT_URL + "/tasks/";
 
   var data = {
     ref: ref || undefined,
@@ -23,25 +46,21 @@ var deployRevision = function(msg) {
     app: app
   };
 
-  console.log("Freight: Making request to %s", url);
+  apiRequest(msg, "/tasks/", "POST", data);
+};
 
-  msg.http(url)
-    .header("Accept", "application/json")
-    .header("Authorization", "Key " + process.env.HUBOT_FREIGHT_API_KEY)
-    .header("Content-Type", "application/json")
-    .post(JSON.stringify(data))(function(err, resp, body) {
-      console.log(resp.statusCode);
-      if (err) {
-        msg.reply("Freight says: " + err);
-        console.error('HTTP %d: %s', resp.statusCode, err);
-      } else if (200 <= resp.statusCode && resp.statusCode < 400) {
-        return;
-      } else {
-        var data = JSON.parse(body);
-        msg.reply("Freight responded with HTTP " + resp.statusCode + ": " + data.error);
-        console.error("HTTP %d: %s", resp.statusCode, body);
-      }
-    });
+var rollbackDeploy = function(msg) {
+  var app = msg.match[1];
+  var env = msg.match[3];
+
+  var data = {
+    ref: ':previous',
+    env: env || undefined,
+    user: msg.message.user.name,
+    app: app
+  };
+
+  apiRequest(msg, "/tasks/", "POST", data);
 };
 
 var cancelDeploy = function(msg) {
@@ -49,35 +68,20 @@ var cancelDeploy = function(msg) {
   var env = msg.match[2];
   var number = msg.match[3];
 
-  var url = process.env.HUBOT_FREIGHT_URL + "/tasks/" + app + "/" + env + "/" + number + "/";
-
   var data = {
     status: "cancelled"
   };
 
-  console.log("Freight: Making request to %s", url);
-
-  msg.http(url)
-    .header("Accept", "application/json")
-    .header("Authorization", "Key " + process.env.HUBOT_FREIGHT_API_KEY)
-    .header("Content-Type", "application/json")
-    .put(JSON.stringify(data))(function(err, resp, body) {
-      if (err) {
-        msg.reply("Freight says: " + err);
-        console.error('HTTP %d: %s', resp.statusCode, err);
-      } else if (200 <= resp.statusCode && resp.statusCode < 400) {
-        return;
-      } else {
-        var data = JSON.parse(body);
-        msg.reply("Freight responded with HTTP " + resp.statusCode + ": " + data.error);
-        console.error("HTTP %d: %s", resp.statusCode, body);
-      }
-    });
+  apiRequest(msg, "/tasks/" + app + "/" + env + "/" + number + "/", "PUT", data);
 };
 
 module.exports = function(robot) {
-  robot.respond(/deploy ([^\s\:]+)(\:([^\s]+))?( to ([^\s]+))?/i, function(msg) {
+  robot.respond(/deploy ([^\s\:\/]+)(\:([^\s]+))?( to ([^\s]+))?/i, function(msg) {
     deployRevision(msg);
+  });
+
+  robot.respond(/rollback ([^\/]+)(\/([^\s]+))?/i, function(msg) {
+    rollbackDeploy(msg);
   });
 
   robot.respond(/cancel ([^\/]+)\/([^#]+)#(\d+)/i, function(msg) {
